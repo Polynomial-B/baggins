bf35cfc9802077aa66fe681cd15ecc6003be1048
Matt <mphlamb@gmail.com>
2024-11-05 11:27:54 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import BackgroundHeader from "./components/BackgroundHeader";
+import Footer from "./components/Footer";
+import "./index.css";
+
+function App() {
+	return (
+		<>
+			<BackgroundHeader />
+			<main></main>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer>
+			<small>&amp; The Shire Studios {new Date().getFullYear()}</small>
+			<p>Version 1.1</p>
+		</footer>
+	);
+}
