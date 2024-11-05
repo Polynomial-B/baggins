84631b23a1d0094856ae438714ddf914fe36942d
Matt <mphlamb@gmail.com>
2024-11-05 12:25:30 +0100

    elements updated

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -1,3 +1,21 @@
+import Button from "./Button";
+
+const secondaryButtons = [
+	"Mark All As Completed",
+	"Mark All As Incompleted",
+	"Undo Changes",
+	"Remove All Items",
+];
 export default function ButtonGroup() {
-	return <div>ButtonGroup</div>;
+	return (
+		<section className="button-group">
+			{secondaryButtons.map((item) => {
+				return (
+					<Button key={item} type="secondary">
+						{item}
+					</Button>
+				);
+			})}
+		</section>
+	);
 }
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,3 +1,20 @@
 export default function ItemList() {
-	return <div>ItemList</div>;
+	return (
+		<ul>
+			<Item item="Item" />
+			<Item item="Item" />
+			<Item item="Item" />
+		</ul>
+	);
+}
+
+function Item({ item }: { item: string }): React.JSX.Element {
+	return (
+		<li className="item">
+			<label>
+				<input type="checkbox"></input>
+				{item}
+			</label>
+		</li>
+	);
 }
