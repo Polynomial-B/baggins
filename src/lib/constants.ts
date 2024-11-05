f21f434b654130a65b7827727c66cc073c033306
Matt <mphlamb@gmail.com>
2024-11-05 14:41:40 +0100

    const file added for default values and buttons

---
+++ src/lib/constants.ts
@@ -0,0 +1,24 @@
+export const initialItems = [
+  {
+    id: 1,
+    item: "A sense of adventure",
+    packed: true,
+  },
+  {
+    id: 2,
+    item: "A gold ring",
+    packed: false,
+  },
+  {
+    id: 3,
+    item: "Lembas bread",
+    packed: false,
+  },
+];
+
+export const secondaryButtons = [
+  "Mark all completed",
+  "Mark all incompleted",
+  "Revert to initial items",
+  "Remove all items",
+];
